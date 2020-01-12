4276e66df8fcf6070d80e7ddef14b92a86eb43dd
StuartSul <ssstuartss@gmail.com>
2020-01-12 21:54:46 +0900

    added sample network models

--- runtests.py
+++ runtests.py
@@ -1,16 +1,16 @@
 from hnn import hnn
 from tests import test_global, test_classification, test_regression
 
-num_hidden_layer = 20;
+num_hidden_layer = 10;
 node_per_layer = 20;
 batch_size = 5;
 learning_rate = .1;
-total_epochs = 500;
-periods = 50;
+total_epochs = 2000;
+periods = 20;
 
 #test_classification.generate(0, 1, 1000)
 #features, labels = test_global.load_data(test_global.default_filename_classification)
-#test_regression.generate(0, 1, 1000)
+test_regression.generate(0, 1, 1000)
 features, labels = test_global.load_data(test_global.dafault_filename_regression)
 
 hnn.init("TEST", len(features[0]), num_hidden_layer,

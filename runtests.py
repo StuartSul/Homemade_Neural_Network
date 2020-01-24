9d884661015a766c20485b13b0c41913dd22c624
StuartSul <ssstuartss@gmail.com>
2020-01-24 21:39:14 +0900

    updated test script

--- runtests.py
+++ runtests.py
@@ -1,21 +1,27 @@
 from hnn.hnn import hnn
+from hnn import util
 from tests import test_global, test_classification, test_regression
 
-num_hidden_layer = 10
-node_per_layer = 20
+features, labels = test_global.load_data(
+            test_regression.generate(0, 1, 2000, save=False),
+            isfile=False)
+
+network_id = 'Test_Network'
+input_count = len(features[0])
+output_count = 1
+structure = [8, 8, 8, 8, 8]
+activation = util.Sigmoid()
+
+train_ratio = 0.8
+loss_function = util.RMSE()
+
 batch_size = 5
 learning_rate = .1
 total_epochs = 2000
 periods = 20
-train_test_ratio = .8
 
-#test_classification.generate(0, 1, 1000)
-#features, labels = test_global.load_data(test_global.default_filename_classification)
-test_regression.generate(0, 1, 1000)
-features, labels = test_global.load_data(test_global.dafault_filename_regression)
+my_hnn = hnn(network_id, input_count, output_count, structure, activation, 
+                        features, labels, train_ratio, loss_function)
 
-my_hnn = hnn("TEST", len(features[0]), num_hidden_layer,
-                node_per_layer, features, labels, train_test_ratio)
-                
 my_hnn.train(batch_size, learning_rate, total_epochs, periods)
            

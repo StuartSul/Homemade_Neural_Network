88ce8cd776f51ca1407b1f9641f084e0d9962a5c
StuartSul <ssstuartss@gmail.com>
2020-01-24 11:35:21 +0900

    classified util functions

--- hnn/util.py
+++ hnn/util.py
@@ -1,37 +1,44 @@
 import math
 
-# Activation Functions
-def sigmoid(x):
-    return 1 / (1 + math.exp(-x))
+class Activation:
+    @staticmethod
+    def sigmoid(x):
+        return 1 / (1 + math.exp(-x))
 
-def tanh(x)
-    return math.tanh(x)
+    @staticmethod
+    def tanh(x)
+        return math.tanh(x)
 
-def ReLU(x)
-    return max([0, x])
+    @staticmethod
+    def ReLU(x)
+        return max([0, x])
 
-# Loss Functions
-def RMSE(predictions, label):
-    loss = 0.0
-    for prediction in predictions:
-        loss += (prediction - label) * (prediction - label)
-    return (loss / len(predictions))**0.5
+class Loss:
+    @staticmethod
+    def RMSE(predictions, label):
+        loss = 0.0
+        for prediction in predictions:
+            loss += (prediction - label) * (prediction - label)
+        return (loss / len(predictions))**0.5
 
-def log_loss(predictions, label):
-    loss = 0.0
-    for prediction in predictions:
-        loss += (label - 1) * math.log(1 - prediction) - label * math.log(prediction)
-    return loss / len(predictions)
+    @staticmethod
+    def log_loss(predictions, label):
+        loss = 0.0
+        for prediction in predictions:
+            loss += (label - 1) * math.log(1 - prediction) - label * math.log(prediction)
+        return loss / len(predictions)
 
-# Regularization
-def L1(weights, lamb):
-    complexity = 0.0
-    for weight in weights:
-        complexity += abs(weight)
-    return lamb * complexity
+class Regularization:
+    @staticmethod
+    def L1(weights, lamb):
+        complexity = 0.0
+        for weight in weights:
+            complexity += abs(weight)
+        return lamb * complexity
 
-def L2(weights, lamb):
-    complexity = 0.0
-    for weight in weights:
-        complexity += weight * weight
-    return lamb * complexity
+    @staticmethod
+    def L2(weights, lamb):
+        complexity = 0.0
+        for weight in weights:
+            complexity += weight * weight
+        return lamb * complexity

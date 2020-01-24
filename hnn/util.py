43617d74401d18ecc038fbb82ad619e4b65b11f5
StuartSul <ssstuartss@gmail.com>
2020-01-24 17:55:44 +0900

    modified derivative functions

--- hnn/util.py
+++ hnn/util.py
@@ -6,7 +6,7 @@ class Linear:
     def calculate(x):
         return x
     @staticmethod
-    def differentiate(x):
+    def derivative(x):
         return 1
 
 class Sigmoid:
@@ -15,7 +15,7 @@ class Sigmoid:
     def calculate(x):
         return 1 / (1 + math.exp(-x))
     @staticmethod
-    def differentiate(x):
+    def derivative(x):
         return Sigmoid.activate(x) * (1 - Sigmoid.activate(x))
 
 class Tanh:
@@ -24,7 +24,7 @@ class Tanh:
     def calculate(x):
         return math.tanh(x)
     @staticmethod
-    def differentiate(x):
+    def derivative(x):
         return 1 - Tanh.activate(x) ** 2
 
 class ReLU:
@@ -33,7 +33,7 @@ class ReLU:
     def calculate(x):
         return max([0, x])
     @staticmethod
-    def differentiate(x):
+    def derivative(x):
         return int(x >= 0)
 
 class RMSE:
@@ -43,26 +43,18 @@ class RMSE:
             loss += (predictions[i] - labels[i]) * (predictions[i] - labels[i])
         loss = (loss / len(predictions))**0.5
         return loss
-    def differentiate(predictions, labels):
-        derivative = 0.0
-        for i in range(len(predictions)):
-            derivative += 2 * (predictions[i] - labels[i])
-        derivative /= len(predictions)
-        return derivative
+    def derivative(prediction, label):
+        return 2 * (prediction - label)
 
-class log_loss:
+class LogLoss:
     def calculate(predictions, labels):
         loss = 0.0
         for i in range(len(predictions)):
             loss += (labels[i] - 1) * math.log(1 - predictions[i]) - labels[i] * math.log(predictions[i])
         loss /= len(predictions)
         return loss
-    def differentiate(predictions, labels):
-        derivative = 0.0
-        for i in range(len(predictions)):
-            derivative += (labels[i] - predictions[i]) / (predictions[i] * (predictions[i] - 1))
-        loss /= len(predictions)
-        return derivative
+    def derivative(prediction, label):
+        return (label - prediction) / (prediction * (prediction - 1))
 
 class L1:
     @staticmethod
